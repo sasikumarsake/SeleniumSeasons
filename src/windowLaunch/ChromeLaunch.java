6cfa7f4df9807529967bd955dfe6089cc0acd1f9
sasikumarsake <108854972+sasikumarsake@users.noreply.github.com>
2025-02-14 10:22:17 +0530

    SeleniumSeasons notes till 7th Feb

---
+++ src/windowLaunch/ChromeLaunch.java
@@ -0,0 +1,22 @@
+package windowLaunch;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeLaunch {
+
+	public static void main(String[] args) {
+		
+		//WebDriverManager.chromedriver().setup();		
+		
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\sasi.kumar\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		
+		
+		 new ChromeDriver();
+
+	}
+
+}
